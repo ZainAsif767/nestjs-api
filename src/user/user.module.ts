6af7e11f136a457bd4fbc28c6b471bd1a2d8b796
ZainAsif767 <zainclasher2002@gmail.com>
2023-10-01 08:48:41 +0500

    user controller

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
 
-@Module({})
+
+@Module({
+  controllers: [UserController],
+})
 export class UserModule {}
