b1ca232f3d203544af5a20c0891267de4d0f4677
ZainAsif767 <zainclasher2002@gmail.com>
2023-10-04 19:38:07 +0500

    validating the user with bearer token

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { JwtGuard } from '../auth/guard';
 
 
 @Controller('users')
 export class UserController {
-    @UseGuards(AuthGuard('jwt'))
+    @UseGuards(JwtGuard)
     @Get('me')
-    getMe() {
-        return 'user info';
+    getMe(@Req() req: Request) {
+        return req.user;
     }
 }
