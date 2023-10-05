f05fff2c9ff7faec59c31af7e9f9dcfbf13a88bd
ZainAsif767 <zainclasher2002@gmail.com>
2023-10-05 16:34:04 +0500

    updated import ( causing fail tests)

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { ConfigService } from '@nestjs/config';
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(
